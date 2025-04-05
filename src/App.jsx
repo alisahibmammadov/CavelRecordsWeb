d6dbef60410238c8bbaf7c12f177909b8bb703d3
alisahibmammadov <ali.yek.10@mail.ru>
2025-04-05 22:52:16 +0400

    link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <main className=''>
-      <h1>Cavel Records</h1>
+    <main className="flex flex-col">
+      <a href="https://www.youtube.com/@HarmoniXI" target="_blank">HarmoniXI Youtube</a>
+      <a href="https://www.youtube.com/@MelodiXI" target="_blank">MelodiXI Youtube</a>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
